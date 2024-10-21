6f518986199980716485f0f5c358237cfb143634
mat <128747016+mocanumatei31@users.noreply.github.com>
2024-10-21 21:23:26 +0300

    Added the Histograms

--- data_gathering.py
+++ data_gathering.py
@@ -38,22 +38,27 @@ for i in correlations.keys():
     print(i)
     print(correlations[i])
 # Generates histograms at dataset level
-'''for i in categories:
-    heights, bins, a = plt.hist(df[i], bins=len(df[i]), color=['blue'], edgecolor='black', rwidth=0.8)
-    plt.title(i)
-    width = bins[1] - bins[0]
-    for j in range(len(df[i])):
-        plt.text(bins[j] + width / 2, heights[j] + 1, str(heights[j]), ha='center')
-    plt.show()'''
+for i in df:
+    if i != 'Plus' and i != 'Row.names' and i != 'Horodateur' and i != 'Race':
+        plt.clf()
+        heights, bins, a = plt.hist(df[i], bins=len(categories[i]), color=['blue'], edgecolor='black', rwidth=0.8)
+        plt.title(i)
+        width = bins[1] - bins[0]
+        bin_centers = (bins[:-1] + bins[1:]) / 2
+        plt.xticks(bin_centers, categories[i])
+        for j in range(len(categories[i])):
+            plt.text(bins[j] + width / 2, heights[j] + 1, str(heights[j]), ha='center')
+        plt.savefig(f"Histograms/DatasetLevel//{i}.png")
 # Generates histograms at race level
 for k in races:
     for i in df:
         if i != 'Plus' and i != 'Row.names' and i != 'Horodateur' and i != 'Race':
+            plt.clf()
             heights, bins, a = plt.hist(df[i][df['Race'] == k], bins=len(race_data[k][i].keys()), color=['blue'], edgecolor='black', rwidth=0.8)
             plt.title(k + i)
             width = bins[1] - bins[0]
             bin_centers = (bins[:-1] + bins[1:]) / 2
             plt.xticks(bin_centers, race_data[k][i].keys())
             for j in range(len(race_data[k][i].keys())):
-                plt.text(bins[j] + width / 2, heights[j] + 1, round(heights[j] / races[k] * 100, 3), ha='center')
-            plt.show()
+                plt.text(bins[j] + width / 2, heights[j] + 1, heights[j], ha='center')
+            plt.savefig(f"Histograms/RaceLevel/{k + i}.png")
